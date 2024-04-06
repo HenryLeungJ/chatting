ea81665dafc3d61faff891be9421ae3f89373a9e
Henry Leung <henryleungj@gmail.com>
2024-04-06 13:36:19 -0700

    navbar and sidebar implemented

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const meeting = ({ params }: { params: { id: string } }) => {
+  return (
+    <div>
+      Meeting Room: #{params.id}
+    </div>
+  )
+}
+
+export default meeting
